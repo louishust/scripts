628504a35cee28a401d4f0d06a73010a73badb91
shuai.lou <louis.hust@gmail.com>
2016-12-04 21:36:02 +0800

    load_csv_parallel.sh : LOAD DATA INFILE WITH PARALLEL

---
+++ load_csv_parallel.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ $# -ne 8 ]
+then
+  echo "Usage: $0 mysql-user mysql-password mysql-host mysql-port mysql-database mysql-table threads csv-file"
+  exit -1
+fi
+
+MYSQL_USER=$1
+MYSQL_PWD=$2
+MYSQL_HOST=$3
+MYSQL_PORT=$4
+MYSQL_DB=$5
+MYSQL_TABLE=$6
+THREADS=$7
+CSV_FILE=$8
+FAIL=0
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+start=$SECONDS
+
+echo -e "${YELLOW}Starting load data from $CSV_FILE into table $MYSQL_DB.$MYSQL_TABLE"
+
+lines=`wc -l $CSV_FILE | awk {'print $1'}`
+chunksize=$((lines/THREADS + 4))
+
+split -a2 -l $chunksize $CSV_FILE mysplit
+
+for file in `find -name "mysplit*"`
+do
+  realfile=`readlink -f $file`
+  mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PWD -P$MYSQL_PORT $MYSQL_DB -A -e "LOAD DATA INFILE '$realfile' INTO TABLE $MYSQL_TABLE" &
+done
+
+for job in `jobs -p`
+do
+  wait $job || let "FAIL+=1"
+done
+
+duration=$(( SECONDS - start ))
+for file in `find -name "mysplit*"`
+do
+  realfile=`readlink -f $file`
+  rm $realfile
+done
+
+if [ "$FAIL" == "0" ];
+then
+  echo -e "${GREEN}LOAD DATA FROM $CSV_FILE INTO $MYSQL_DB.$MYSQL_TABLE Finished!"
+  echo -e "${GREEN}Time Cost ($duration) seconds"
+else
+  echo -e "${RED}LOAD DATA FAIL($FAIL)! "
+fi
